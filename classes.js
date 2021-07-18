d446fe6192db83dea273b22ba56f53e272595583
deivson <dayvson04@hotmail.com>
2021-07-17 23:27:31 -0300

    alteraco no projeto heranca

--- classes.js
+++ classes.js
@@ -1,4 +1,19 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Data = /** @class */ (function () {
     function Data(dia, mes, ano) {
         this.dia = dia;
@@ -10,3 +25,49 @@ var Data = /** @class */ (function () {
 var data = new Data(1, 1, 2020);
 console.log(data.dia);
 var data2 = new Data(1, 1, 1);
+var Carro = /** @class */ (function () {
+    function Carro(marca, modelo, velocidadeMaxima) {
+        if (velocidadeMaxima === void 0) { velocidadeMaxima = 220; }
+        this.marca = marca;
+        this.modelo = modelo;
+        this.velocidadeMaxima = velocidadeMaxima;
+        this.velocidadeAtual = 0;
+    }
+    Carro.prototype.alterarVelocidade = function (delta) {
+        // faco as validaçãoes de aceleração e frenagem
+        // this. velocidadeAtaual = xxx
+        var novaVelocidade = this.velocidadeAtual + delta;
+        if (novaVelocidade >= 0 && novaVelocidade <= this.velocidadeMaxima) {
+            this.velocidadeAtual = novaVelocidade;
+        }
+        else {
+            this.velocidadeAtual = delta > 0 ? this.velocidadeMaxima : 0;
+        }
+    };
+    Carro.prototype.acelerar = function () {
+        this.alterarVelocidade(5);
+    };
+    Carro.prototype.frear = function () {
+        this.alterarVelocidade(-5);
+    };
+    return Carro;
+}());
+var carro = new Carro('Chevrolet', 'Prisma', 250);
+carro.acelerar();
+// Herença
+var Camaro = /** @class */ (function (_super) {
+    __extends(Camaro, _super);
+    function Camaro() {
+        var _this = _super.call(this, 'Checrolet', 'Camaro', 500) || this;
+        _this.turbo = false;
+        return _this;
+    }
+    Camaro.prototype.ligarTurbo = function () {
+        this.turbo = true;
+    };
+    return Camaro;
+}(Carro));
+var camaro = new Camaro();
+camaro.acelerar();
+camaro.frear();
+camaro.ligarTurbo();
--- classes.ts
+++ classes.ts
@@ -21,3 +21,67 @@ console.log(data.dia);
 
 const data2 = new Data(1, 1, 1);
 
+ class Carro {
+    private velocidadeAtual: number = 0; 
+    
+    constructor(
+         public marca: string,
+         public modelo: string,
+         private velocidadeMaxima: number = 220
+     ) {}
+ 
+     private alterarVelocidade(delta:number) {
+         // faco as validaçãoes de aceleração e frenagem
+         // this. velocidadeAtaual = xxx
+         const novaVelocidade = this.velocidadeAtual + delta;
+         
+         if(novaVelocidade >= 0 && novaVelocidade <= this.velocidadeMaxima) {
+             this.velocidadeAtual = novaVelocidade;   
+         }
+         else {
+             this.velocidadeAtual = delta > 0 ? this.velocidadeMaxima : 0
+         }
+     
+        }
+
+     acelerar() {
+         this.alterarVelocidade(5);
+
+     }
+
+     frear( ) {
+         this.alterarVelocidade(-5);
+
+      }
+
+    } 
+
+    const carro = new Carro('Chevrolet', 'Prisma', 250);
+    carro.acelerar();
+
+
+    // Herença
+
+    class Camaro extends Carro {
+        private turbo = false;
+        
+        constructor() {
+            super('Checrolet', 'Camaro', 500);
+        
+        }
+            ligarTurbo() {
+                this.turbo = true;
+            }    
+        
+        }
+
+
+
+    const camaro = new Camaro();
+    camaro.acelerar()
+    camaro.frear();
+    camaro.ligarTurbo();
+
+    
+
+    
